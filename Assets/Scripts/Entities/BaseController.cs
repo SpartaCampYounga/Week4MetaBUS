dd61d6a8ec1fec34989a8c3db3145912b86120d9
Younga <youngajin93@gmail.com>
2025-07-24 20:14:48 +0900

    [fix]방향키 바꾸기
    화살표방향키에서 wasd로 바꿈

--- Assets/Scripts/Entities/BaseController.cs
+++ Assets/Scripts/Entities/BaseController.cs
@@ -40,7 +40,7 @@ public class BaseController : MonoBehaviour
 
         // 실제 물리 이동
         _rigidbody.velocity = direction;
-        
+        Debug.Log(direction.x);
         Rotate(movementDirection);  //이동할때만 방향 바뀔거라서 Movement 있을 때만 확인. zep처럼
     }
 
