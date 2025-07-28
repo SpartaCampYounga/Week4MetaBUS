72963fdbd52a48eb622d9264a0e4589dc232ef6e
Younga <youngajin93@gmail.com>
2025-07-28 12:30:28 +0900

    [fix] 길 만듬
    길 못 찾을 수도 있어서, 그냥 길 만듬!

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -1,9 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    
-
+    //이미지 변경 할 수 있도록 구현하면 좋을듯.
 }
